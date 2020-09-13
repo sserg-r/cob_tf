013e52ebc95fb49866c39c5e9ba3e83b4acc71f3
sserg-r <rusetskyserg@gmail.com>
2020-09-14 00:03:39 +0300

    2020-09-14 00:03

--- python/.ipynb_checkpoints/segments-checkpoint.py
+++ python/.ipynb_checkpoints/segments-checkpoint.py
@@ -66,6 +66,52 @@ def get_l_p(L):
     
     return {'l': l0-a0,'p':b0+p0, 'n': n, 'res':diff}
 
+def batcherize (image):
+    '''divide a 3-dims image with the sum of 2 dimensions more than 1000 into n patches with the size of h or w no more than 500'''
+    h,w,_=image.shape    
+    out_batch=[]    
+    if h+w<=1000:
+        out_batch.append(image)
+    else:
+        par_h=get_l_p(h)
+        par_w=get_l_p(w)
+        tim_padded=np.pad(image, ((0,par_h['res']),(0,par_w['res']),(0,0)),mode='symmetric')
+        step_h=par_h['l']-par_h['p']
+        step_w=par_w['l']-par_w['p']        
+        for ih in range(par_h['n']):
+            for iw in range( par_w['n']):  
+                out_batch.append(tim_padded[ih*step_h:ih*step_h+par_h['l'],iw*step_w:iw*step_w+par_w['l'],:])
+    
+    overlapping=(par_h['p'], par_w['p'])
+    res=(par_h['res'], par_w['res'])
+    n=(par_h['n'],par_w['n'])
+    return out_batch, overlapping, n, res
+
+def merger (batch, overlapping, n, res):
+    
+    '''merges batch of images in 1 image taking into account number of patches in h & w dims, 
+    image's overlapping and trim resulting image right and bottom according to the res'''
+    
+    N, h,w,b=batch.shape
+    
+    step_h=h-overlapping[0]
+    step_w=w-overlapping[1]
+    
+    
+    L_h=h*n[0]-(n[0]-1)*overlapping[0]
+    L_w=w*n[1]-(n[1]-1)*overlapping[1]
+    
+    out=np.zeros((L_h,L_w,b))
+    ittr=iter(batch)
+
+    for ih in range(n[0]):
+        shift_h=overlapping[0]//2 if ih!=0 else 0 
+        for iw in range( n[1]):
+            shift_w=overlapping[1]//2 if iw!=0 else 0                         
+            out[ih*step_h+shift_h:ih*step_h+h,iw*step_w+shift_w:iw*step_w+w,:]=next(ittr)[shift_h:,shift_w:,:]
+            
+    return out[:L_h-res[0],:L_w-res[1],:]
+
 # def get_l_p(L):
 #     import math
 #     p0=100
